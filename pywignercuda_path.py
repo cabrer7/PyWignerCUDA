0b4c8e175dc3e605ffee4e309bde1a16187472ff
Renan <cabrer7@gmail.com>
2015-09-29 16:09:59 -0400

    Added pywignercuda_path.py

---
+++ pywignercuda_path.py
@@ -0,0 +1,7 @@
+#!/usr/local/epd/bin/python
+#-----------------------------------------------------------
+#                
+#-----------------------------------------------------------
+def SetPyWignerCUDA_Path():
+	import sys
+	sys.path.append("/home/rcabrera/Documents/source/python/PyWignerCUDA")
